d632664e8bd1b3f4e7ddb7e2baed7548bbaedcce
Parker Melling <pmelling3@gatech.edu>
2026-09-11 16:41:27 -0400

    Chapter 2 complete, 1/3 chapter 3 complete

--- moviesstore/home/urls.py
+++ moviesstore/home/urls.py
@@ -3,4 +3,6 @@ from . import views
 urlpatterns = [
   path('', views.index, name='home.index'),
   # '' = url path, views.index = HTTP handler function, home.index = url pattern id
+  path('about', views.about, name='home.about'),
+  # 'about' = url path, views.about = HTTP handler function, home.about = url pattern id
 ]
--- moviesstore/home/views.py
+++ moviesstore/home/views.py
@@ -4,3 +4,7 @@ def index(request):
     # renders templates & returns HTTP response  w/ content
     return render(request, 'home/index.html')
     # request = HTTP request, home/index.html =  path to template file
+    
+# Same as index function, except different template file
+def about(request):
+    return render(request, 'home/about.html')
