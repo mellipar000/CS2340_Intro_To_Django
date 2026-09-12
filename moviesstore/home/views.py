572b51e86f7aa9f99ab59325fadc8db8afaba81e
Parker Melling <pmelling3@gatech.edu>
2026-09-12 14:51:38 -0400

    Completed Chapter 3

--- moviesstore/home/views.py
+++ moviesstore/home/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 
 def index(request):
+    template_data = {}
+    template_data['title'] = 'Movies Store'
     # renders templates & returns HTTP response  w/ content
-    return render(request, 'home/index.html')
-    # request = HTTP request, home/index.html =  path to template file
+    return render(request, 'home/index.html', 
+                  {'template_data': template_data}) # template_data = browser tab title
+    # request = HTTP request, home/index.html = path to template file
     
 # Same as index function, except different template file
 def about(request):
-    return render(request, 'home/about.html')
+    template_data = {}
+    template_data['title'] = 'About'
+    return render(request, 'home/about.html', 
+                  {'template_data': template_data})
