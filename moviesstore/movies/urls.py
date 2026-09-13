1eb1343deb4566aa7fe4504d5e193da5ea6d6668
Parker Melling <pmelling3@gatech.edu>
2026-09-13 14:44:37 -0400

    Chapter 9 Completed

--- moviesstore/movies/urls.py
+++ moviesstore/movies/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     # If movies path matched, execute the views.index() function
     path('<int:id>/', views.show, name='movies.show'),
     # <ind:id> = expects integer to be passed from the URL for the movie id
+    path('<int:id>/review/create/', views.create_review, name='movies.create_review'),
+    path('<int:id>/review/<int:review_id>/edit/', views.edit_review, name='movies.edit_review'),
+    path('<int:id>/review/<int:review_id>/delete/', views.delete_review, name='movies.delete_review'),
 ]
